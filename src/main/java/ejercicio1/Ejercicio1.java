b26d9567f85e3417397554cf1b2ff2f4f3385ef4
cristinaj1 <cristinacee305@gmail.com>
2021-04-08 11:50:07 +0200

    Ejercicio 5 hecho y corregido

--- src/main/java/ejercicio1/Ejercicio1.java
+++ src/main/java/ejercicio1/Ejercicio1.java
@@ -8,6 +8,7 @@ package ejercicio1;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -19,76 +20,69 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-
+        // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
         String idFichero = "matriz.txt";
         String tmp;
         String[] tokens;
+        int valor = 0;
+        int total = 0;
         String linea;
+        // Array a escribir
+        int matrizNumeros[][] = new int[5][8];
 
-        int[][] matrizNumeros = new int[5][8];
+        // Si se utiliza el constructor FileWriter(idFichero, true) entonces se anexa información
+        // al final del fichero idFichero
+        // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
+        // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
         int inicial = 100;
-        int resultadoFila = 0;
-        int resultadoTotal = 0;
-        
-        //Para que se cree
+        String primerValor = "";
+
         try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
             for (int i = 0; i < matrizNumeros.length; i++) {
                 inicial = 100 * (i + 1);
                 matrizNumeros[i][0] = inicial;
-
                 for (int j = 0; j < matrizNumeros[i].length; j++) {
                     matrizNumeros[i][j] = inicial + j;
                     // Obtengo en un String el elemento int de la matriz
                     tmp = String.valueOf(matrizNumeros[i][j]);
-                    resultadoFila += matrizNumeros[i][j];
-                    flujo.write(tmp + "\t");
-                }
 
+                    flujo.write(tmp + "\t");
+
+                }
                 // Metodo newLine() añade salto de línea después de cada fila
                 flujo.newLine();
-                System.out.println("El resultado es: " + resultadoFila);
-                
             }
             // Metodo fluh() guarda cambios en disco 
             flujo.flush();
             System.out.println("Fichero " + idFichero + " creado correctamente.");
-
-            //Es un try with resources y sirve para cerrar todos los recursos 
-            //que estamos usando cuando acabamos de usarlo(automáticamente)
-            //Debido a que consume espacio y memoria
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        try ( Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
 
-        //Para que se muestre
-        System.out.println("Leyendo el proyecto " + idFichero);
+            System.out.println("Separando cada elemento del fichero: ");
 
-        try ( Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
-            // hasNextLine devuelve true mientras haya líneas por leer
             while (datosFichero.hasNextLine()) {
-                // Guarda la línea completa en un String
+
                 linea = datosFichero.nextLine();
                 // Se guarda en el array de String cada elemento de la
-                // línea en función del carácter separador de campos del fichero CSV
-                tokens = linea.split(";");
+                // línea en función del carácter separador coma
+                tokens = linea.split("\t");
+                for (int i = 0; i < tokens.length; i++) {
+                    valor = Integer.parseInt(tokens[i]);
+                    total += valor;
+                }
                 for (String string : tokens) {
                     System.out.print(string + "\t");
                 }
                 System.out.println();
-                for (int i = 0; i < matrizNumeros.length; i++) {
-                for (int j = 0; j < matrizNumeros[i].length; j++) {
-                    resultadoFila += matrizNumeros[i][j];
-                }
-                for (int k = 0; k < resultadoFila; k++) {
-                    resultadoTotal += resultadoFila;
-
-                }
-                }
             }
-                System.out.println("resultado total " + resultadoTotal);
-            
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("La suma de todos los numeros es de: " + total);
+
     }
 }
+
+        
